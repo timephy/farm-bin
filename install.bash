6670aca5fb923a76b0b970fee17b833e5ad03cb7
Tim Guggenmos <tim.guggenmos@me.com>
2021-07-28 18:31:33 +0200

    fixes

--- install.bash
+++ install.bash
@@ -1,13 +1,15 @@
-cd ~
+#!/bin/bash
 
+cd ~
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
-cd ~/chia-blockchain
+cd chia-blockchain
 sh install.sh
 . ./activate
 chia init
 chia keys add -f ~/mnemonic
 deactivate
 
+cd ~
 git clone https://github.com/Flax-Network/flax-blockchain.git -b latest --recurse-submodules
 cd ~/flax-blockchain
 sh install.sh
@@ -16,16 +18,18 @@ flax init
 flax keys add -f ~/mnemonic
 deactivate
 
+cd ~
 git clone https://github.com/ChainGreenOrg/chaingreen-blockchain.git -b latest --recurse-submodules
-cd ~/chaingreen-blockchain
+cd chaingreen-blockchain
 sh install.sh
 . ./activate
 chaingreen init
 chaingreen keys add -f ~/mnemonic
 deactivate
 
+cd ~
 git clone https://github.com/silicoin-network/silicoin-blockchain.git -b latest --recurse-submodules
-cd ~/silicoin-blockchain
+cd silicoin-blockchain
 sh install.sh
 . ./activate
 silicoin init
