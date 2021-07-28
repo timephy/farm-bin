88a1b349601ff8b5fcd463a5b008173dad40b5fa
Tim Guggenmos <tim.guggenmos@me.com>
2021-07-28 21:17:48 +0200

    fixes, color, typo

--- install.bash
+++ install.bash
@@ -1,57 +1,60 @@
 #!/bin/bash
 
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 cd ~
-echo Git clone chia
+echo -e "${BLUE}$ git clone chia${CL}"
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd chia-blockchain
-echo $ sh install.sh
+echo -e "${BLUE}$ sh install.sh${CL}"
 sh install.sh
 . ./activate
-echo $ chia init
+echo -e "${BLUE}$ chia init${CL}"
 chia init
-echo $ chia keys add -f ~/mnemonic
+echo -e "${BLUE}$ chia keys add -f ~/mnemonic${CL}"
 chia keys add -f ~/mnemonic
 deactivate
 echo
 
 cd ~
-echo Git clone flax
+echo -e "${BLUE}$ git clone flax${CL}"
 git clone https://github.com/Flax-Network/flax-blockchain.git -b latest --recurse-submodules
 cd ~/flax-blockchain
-echo $ sh install.sh
+echo -e "${BLUE}$ sh install.sh${CL}"
 sh install.sh
 . ./activate
-echo $ flax init
+echo -e "${BLUE}$ flax init${CL}"
 flax init
-echo $ flax keys add -f ~/mnemonic
+echo -e "${BLUE}$ flax keys add -f ~/mnemonic${CL}"
 flax keys add -f ~/mnemonic
 deactivate
 echo
 
 cd ~
-echo Git clone chaingreen
+echo -e "${BLUE}$ git clone chaingreen${CL}"
 git clone https://github.com/ChainGreenOrg/chaingreen-blockchain.git -b main --recurse-submodules
 cd chaingreen-blockchain
-echo $ sh install.sh
+echo -e "${BLUE}$ sh install.sh${CL}"
 sh install.sh
 . ./activate
-echo $ chaingreen init
+echo -e "${BLUE}$ chaingreen init${CL}"
 chaingreen init
-echo $ chaingreen keys add -f ~/mnemonic
+echo -e "${BLUE}$ chaingreen keys add -f ~/mnemonic${CL}"
 chaingreen keys add -f ~/mnemonic
 deactivate
 echo
 
 cd ~
-echo Git clone silicoin
+echo -e "${BLUE}$ git clone silicoin${CL}"
 git clone https://github.com/silicoin-network/silicoin-blockchain.git -b main --recurse-submodules
 cd silicoin-blockchain
-echo $ sh install.sh
+echo -e "${BLUE}$ sh install.sh${CL}"
 sh install.sh
 . ./activate
-echo $ silicoin init
+echo -e "${BLUE}$ silicoin init${CL}"
 silicoin init
-echo $ silicoin keys add -f ~/mnemonic
+echo -e "${BLUE}$ silicoin keys add -f ~/mnemonic${CL}"
 silicoin keys add -f ~/mnemonic
 deactivate
 echo
