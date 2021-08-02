7ce1b7d1d64c51f29770302a2c11512feabec159
Tim Guggenmos <tim.guggenmos@me.com>
2021-08-02 14:55:49 +0200

    telegram notify and check for not farmign

--- send_telegram_notifications.py
+++ send_telegram_notifications.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 import subprocess
 import requests
+import sys
 
 TELEGRAM_TOKEN = ''
 TELEGRAM_CHAT_ID = ''
+
+NOTIFY = 'notify' in sys.argv
 
 coins = ['chia', 'flax', 'chaingreen', 'silicoin']
 
@@ -13,14 +16,27 @@ coins = ['chia', 'flax', 'chaingreen', 'silicoin']
 def get_for_coin(coin, command):
     cmd = f'~/{coin}-blockchain/venv/bin/{coin} {command}'
     result = subprocess.check_output(cmd, shell=True, text=True)
-    return f'*{coin.capitalize()}*\n{result}'
+    return result
+
+
+def send_telegram(text, parse_mode='markdown'):
+    requests.post(
+        f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage',
+        params={'chat_id': TELEGRAM_CHAT_ID, 'text': text, 'parse_mode': parse_mode})
 
 
 results = [get_for_coin(coin, 'farm summary') for coin in coins]
 
-result = '\n'.join(results)
-print(result)
+# Check
+for coin, result in zip(coins, results):
+    if 'Farming status: Farming' not in result:
+        send_telegram(f'❗️ {coin.capitalize()} not farming!\n{result}')
 
-requests.post(
-    f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage',
-    params={'chat_id': TELEGRAM_CHAT_ID, 'text': result, 'parse_mode': 'markdown'})
+
+# Notify
+if NOTIFY:
+    result = '\n'.join([f'🌱 *{coin.capitalize()}*\n{result}' for
+                        coin, result in zip(coins, results)])
+    print(result)
+
+    send_telegram(result)
