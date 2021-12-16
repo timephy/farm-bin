f52fb0c98e68ad00de47c387455e1f71a9422173
Tim Guggenmos <tim.guggenmos@me.com>
2021-12-16 10:11:20 +0100

    fix restart

--- restart_coin.bash
+++ restart_coin.bash
@@ -1,8 +1,10 @@
-source ~/farm-bin/functions.bash
+. ~/$1-blockchain/activate
 
 echo -e "${BLUE}$ $1 stop all -d${NC}"
-coin $1 stop all -d
+$1 stop all -d
 echo -e "${BLUE}$ pkill -9 $1${NC}"
 pkill -9 $1
 echo -e "${BLUE}$ $1 start $2${NC}"
-coin $1 start $2
+$1 start $2
+
+deactivate
